39cb705c1a606240577f5ef3323fc7b571fadc6d
Ajay <ajaykewat0000@gmail.com>
2020-11-13 11:11:26 +0530

    added build

---
+++ build/precache-manifest.2e069919bbaf91fa997d9d683e1340cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcdbc24a7f91d86281eda59a352116d",
+    "url": "/src/components/WeatherEngine.js/index.html"
+  },
+  {
+    "revision": "53f99371c32ec9ce8205",
+    "url": "/src/components/WeatherEngine.js/static/css/main.13bc2b54.chunk.css"
+  },
+  {
+    "revision": "8e61901f653157e78529",
+    "url": "/src/components/WeatherEngine.js/static/js/2.4432204a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/src/components/WeatherEngine.js/static/js/2.4432204a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f99371c32ec9ce8205",
+    "url": "/src/components/WeatherEngine.js/static/js/main.31b9f42d.chunk.js"
+  },
+  {
+    "revision": "64cd92bc29054ef736ad",
+    "url": "/src/components/WeatherEngine.js/static/js/runtime-main.93a388ad.js"
+  }
+]);
